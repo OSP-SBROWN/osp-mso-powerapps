f4f689305065bd7194330168f95476cbaca6ea66
Shaun Brown <126246233+OSP-SBROWN@users.noreply.github.com>
2025-10-14 22:10:32 +0100

    Disconnected from Power Platform. Saving changes for sanity!
    Hierarchy Management Component only partly workign, and looking rubbish. Need to fix tomnorrow. Sort out cascading values, and branding.

--- SQL/MSO-DB-v6/StoredProcedures/HierMan2_GetFullHierarchy.sql
+++ SQL/MSO-DB-v6/StoredProcedures/HierMan2_GetFullHierarchy.sql
@@ -1,5 +1,5 @@
 
-CREATE   PROCEDURE [ui].[HierMan2_GetFullHierarchy]
+CREATE  PROCEDURE [ui].[HierMan2_GetFullHierarchy]
     @SQL_Success  BIT OUTPUT,
     @SQL_Message  NVARCHAR(4000) OUTPUT,   -- more room than 255
     @Hier_Version INT
@@ -46,33 +46,75 @@ BEGIN
         SET @c_detail = @@ROWCOUNT;
 
         /* =========================================
-           2) Hierarchy1_Child view (Result set #2)
+           2) Hierarchy1 with child counts (Result set #2)
            ========================================= */
         SELECT
-            h1c.Hierarchy1_ID,
-            h1c.Hierarchy1_Code,
-            h1c.Hierarchy1_Name,
-            h1c.Child_H2,
-            h1c.Child_H3,
-            h1c.HierarchyVersion_ID
-        FROM dbo.Hierarchy1_Child AS h1c
-        WHERE h1c.HierarchyVersion_ID = @Hier_Version
-        ORDER BY h1c.Hierarchy1_Code;
+            h1.Hierarchy1_ID,
+            h1.Hierarchy1_Code,
+            h1.Hierarchy1_Name,
+            h1.Trend,
+            h1.MinBays,
+            h1.MaxBays,
+            h1.DOS AS DaysOnSite,
+            h1.COS AS CapacityOnSite,
+            h1.Exclude_From_Analysis AS Exclude,
+            h1.BayRoundingThreshold,
+            h1.Metric1 AS Metric1_Pct,
+            h1.Metric2 AS Metric2_Pct,
+            h1.Metric3 AS Metric3_Pct,
+            h1.Metric4 AS Metric4_Pct,
+            h1.Driver1 AS Driver1_Pct,
+            h1.Driver2 AS Driver2_Pct,
+            h1.Driver3 AS Driver3_Pct,
+            h1.Driver4 AS Driver4_Pct,
+            h1.HierarchyVersion_ID,
+            COUNT(DISTINCT h2.Hierarchy2_ID) AS Child_H2,
+            COUNT(DISTINCT h3.Hierarchy3_ID) AS Child_H3
+        FROM dbo.Hierarchy1 AS h1
+        LEFT JOIN dbo.Hierarchy2 AS h2 ON h2.ParentHierarchy_ID = h1.Hierarchy1_ID
+        LEFT JOIN dbo.Hierarchy3 AS h3 ON h3.ParentHierarchy_ID = h2.Hierarchy2_ID
+        WHERE h1.HierarchyVersion_ID = @Hier_Version
+        GROUP BY h1.Hierarchy1_ID, h1.Hierarchy1_Code, h1.Hierarchy1_Name, h1.Trend, h1.MinBays, h1.MaxBays, 
+                 h1.DOS, h1.COS, h1.Exclude_From_Analysis, h1.BayRoundingThreshold,
+                 h1.Metric1, h1.Metric2, h1.Metric3, h1.Metric4,
+                 h1.Driver1, h1.Driver2, h1.Driver3, h1.Driver4, h1.HierarchyVersion_ID
+        ORDER BY h1.Hierarchy1_Code;
         SET @c_h1 = @@ROWCOUNT;
 
         /* =========================================
-           3) Hierarchy2_Child view (Result set #3)
+           3) Hierarchy2 with all fields (Result set #3)
            ========================================= */
         SELECT
-            h2c.Hierarchy2_ID,
-            h2c.Hierarchy2_Code,
-            h2c.Hierarchy2_Name,
-            h2c.ParentHierarchy_ID,
-            h2c.Child_H3,
-            h2c.HierarchyVersion_ID
-        FROM dbo.Hierarchy2_Child AS h2c
-        WHERE h2c.HierarchyVersion_ID = @Hier_Version
-        ORDER BY h2c.ParentHierarchy_ID, h2c.Hierarchy2_Code;
+            h2.Hierarchy2_ID,
+            h2.Hierarchy2_Code,
+            h2.Hierarchy2_Name,
+            h2.Trend,
+            h2.MinBays,
+            h2.MaxBays,
+            h2.DOS AS DaysOnSite,
+            h2.COS AS CapacityOnSite,
+            h2.Exclude_From_Analysis AS Exclude,
+            h2.BayRoundingThreshold,
+            h2.Metric1 AS Metric1_Pct,
+            h2.Metric2 AS Metric2_Pct,
+            h2.Metric3 AS Metric3_Pct,
+            h2.Metric4 AS Metric4_Pct,
+            h2.Driver1 AS Driver1_Pct,
+            h2.Driver2 AS Driver2_Pct,
+            h2.Driver3 AS Driver3_Pct,
+            h2.Driver4 AS Driver4_Pct,
+            h2.ParentHierarchy_ID,
+            h2.HierarchyVersion_ID,
+            COUNT(DISTINCT h3.Hierarchy3_ID) AS Child_H3
+        FROM dbo.Hierarchy2 AS h2
+        LEFT JOIN dbo.Hierarchy3 AS h3 ON h3.ParentHierarchy_ID = h2.Hierarchy2_ID
+        WHERE h2.HierarchyVersion_ID = @Hier_Version
+        GROUP BY h2.Hierarchy2_ID, h2.Hierarchy2_Code, h2.Hierarchy2_Name, h2.Trend, h2.MinBays, h2.MaxBays,
+                 h2.DOS, h2.COS, h2.Exclude_From_Analysis, h2.BayRoundingThreshold,
+                 h2.Metric1, h2.Metric2, h2.Metric3, h2.Metric4,
+                 h2.Driver1, h2.Driver2, h2.Driver3, h2.Driver4,
+                 h2.ParentHierarchy_ID, h2.HierarchyVersion_ID
+        ORDER BY h2.ParentHierarchy_ID, h2.Hierarchy2_Code;
         SET @c_h2 = @@ROWCOUNT;
 
         /* =========================================
@@ -86,11 +128,19 @@ BEGIN
             h3.Trend,
             h3.MinBays,
             h3.MaxBays,
-            h3.DOS,
-            h3.COS,
-            h3.Exclude_From_Analysis,
+            h3.DOS AS DaysOnSite,
+            h3.COS AS CapacityOnSite,
+            h3.Exclude_From_Analysis AS Exclude,
             h3.FlowNumber,
             h3.BayRoundingThreshold,
+            h3.Metric1 AS Metric1_Pct,
+            h3.Metric2 AS Metric2_Pct,
+            h3.Metric3 AS Metric3_Pct,
+            h3.Metric4 AS Metric4_Pct,
+            h3.Driver1 AS Driver1_Pct,
+            h3.Driver2 AS Driver2_Pct,
+            h3.Driver3 AS Driver3_Pct,
+            h3.Driver4 AS Driver4_Pct,
             h3.HierarchyVersion_ID
         FROM dbo.Hierarchy3 AS h3
         WHERE h3.HierarchyVersion_ID = @Hier_Version
